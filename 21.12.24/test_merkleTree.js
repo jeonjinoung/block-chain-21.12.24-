500bd8fa52d5fe88e7e389c579502edee7b133cd
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2021-12-24 17:13:04 +0900

    merkleTree.js SHA256 MerkleTree

---
+++ 21.12.24/test_merkleTree.js
@@ -0,0 +1,33 @@
+// test_merkleTree.js
+//
+const {MerkleTree} = require('merkletreejs')
+
+const SHA256 = require('crypto-js/sha256')
+//console.log(SHA256('a').toString())
+//
+
+const testSet = ['a', 'b', 'c', 'd', 'e']
+const testArray = testSet.map((v)=>SHA256(v).toString())
+
+//console.log(testArray)
+
+const testMerkleTree = new MerkleTree(testArray, SHA256)
+console.log(testMerkleTree)
+
+const merkleRoot = testMerkleTree.getRoot()
+console.log(merkleRoot.toString('hex'))
+
+const testValue = 'u'
+const leaf = SHA256(testValue)
+const proof = testMerkleTree.getProof(leaf)
+console.log(proof)
+
+const result = testMerkleTree.verify(proof, leaf, merkleRoot)
+console.log(result)
+
+
+
+
+
+
+
